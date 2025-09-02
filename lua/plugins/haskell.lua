316a3ce4a6e24c6a2130b0099db389b20b2665d9
Stephen Onnen <stephen.onnen@gmail.com>
2025-09-01 22:57:43 -0500

    add extras

--- lua/plugins/haskell.lua
+++ lua/plugins/haskell.lua
@@ -2,8 +2,6 @@ return {{
     'mrcjkb/haskell-tools.nvim',
     version = '^6', -- Recommended
     config = function()
-        local wk = require("which-key")
-
         local ht = require('haskell-tools')
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = {
@@ -12,17 +10,6 @@ return {{
             buffer = bufnr
         }
 
-        wk.add({{
-            "<leader>cz",
-            ht.repl.toggle,
-            desc = "Toggle a GHCi repl",
-            opts
-        }, {
-            "<leader>cZ",
-            ht.repl.quit,
-            desc = "Quit GHCi repl",
-            opts
-        }})
         require('telescope').load_extension('ht')
     end,
     lazy = false -- This plugin is already lazy
