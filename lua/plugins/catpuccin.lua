6f7b979ac039f1cc287ef3c2334e30ebe7dd49b7
Stephen Onnen <stephen.onnen@gmail.com>
2025-09-01 19:05:24 -0500

    init

---
+++ lua/plugins/catpuccin.lua
@@ -0,0 +1,19 @@
+return {{
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            auto_integrations = true
+        })
+        vim.cmd.colorscheme "catppuccin"
+    end
+}, {
+    "akinsho/bufferline.nvim",
+    after = "catppuccin",
+    config = function()
+        require("bufferline").setup {
+            highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+        }
+    end
+}}
