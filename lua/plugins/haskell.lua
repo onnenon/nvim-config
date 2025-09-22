becf8814b386a81d3135da992177d48aea3c3f4c
Stephen Onnen <stephen.onnen@gmail.com>
2025-09-22 16:56:46 -0500

    working things

--- lua/plugins/haskell.lua
+++ lua/plugins/haskell.lua
@@ -14,12 +14,5 @@ return {{
     lazy = false -- This plugin is already lazy
 }, {
     "mrcjkb/haskell-snippets.nvim",
-    dependencies = {"L3MON4D3/LuaSnip"},
-    ft = {"haskell", "lhaskell", "cabal", "cabalproject"},
-    config = function()
-        local haskell_snippets = require("haskell-snippets").all
-        require("luasnip").add_snippets("haskell", haskell_snippets, {
-            key = "haskell"
-        })
-    end
+    enabled = false
 }}
